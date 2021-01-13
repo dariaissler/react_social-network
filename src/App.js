bba4d5411a73eccbe5bda2727700195077447aaa
Daria <daria.bycova@yandex.ru>
2021-01-13 11:42:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
+import Friends from './Components/Friends/Friends';
+import {Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = (props) => {
+  return(
+<div className='app-wrapper'>
+      
+      <Header/>
+      <Navbar/>
+      <div className='app-wrapper-content'>
+        {/* <Route path='/dialogs' component={Dialogs}/>
+        <Route path='/profile' component={Profile}/> */}
+
+        <Route path='/dialogs' render={ () => <Dialogs sendMessage={props.sendMessage} dialogs={props.dialogs} messages={props.messages}/>} />
+        <Route path='/profile' render={ () => <Profile updateNewPostText={props.updateNewPostText} newPostText={props.posts.newPostText} addPost={props.addPost} posts={props.posts}/>} />    
+        <Route path='/friends' render={ () => <Friends friends={props.friends}/>} />
+        
+      
+      </div>
+      </div>
+      
+
+      
+  
+ 
+     
+     
+   
+
+   
+
   );
-}
+
+};
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state, {subscribe} from './Components/redux/State';
+import {addPost, updateNewPostText, sendMessage} from './Components/redux/State';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// addPost('hello you bitches how is your fucking lifes? ');
+
+export let renderEntireTree =(state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+          <App updateNewPostText={updateNewPostText} sendMessage={sendMessage} addPost={addPost} dialogs={state.dialogs} messages={state.messages} posts={state.posts} friends={state.friends}/>, 
+        </BrowserRouter>,
+      
+        document.getElementById('root')
+      );
+}
+renderEntireTree(state);
+subscribe(renderEntireTree);
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App addPost={addPost} dialogs={state.dialogs} messages={state.messages} posts={state.posts} friends={state.friends}/>, 
+//   </BrowserRouter>,
+
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
